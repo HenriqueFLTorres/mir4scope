43c7bf3b112823b9693e3a76bff42e76f22c094b
HenriqueFLTorres <ricalisboa@gmail.com>
2024-02-28 15:42:45 -0300

    fix - clear api not properly deleting all the data

--- src/app/api/clear/route.ts
+++ src/app/api/clear/route.ts
@@ -3,13 +3,11 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   try {
-    await prisma.nft.deleteMany({
-      where: {
-        nft_id: {
-          gte: 1
-        }
-      }
-    });
+    await prisma.nft.deleteMany();
+    await prisma.assets.deleteMany();
+    await prisma.spirit.deleteMany();
+    await prisma.genericStat.deleteMany();
+    await prisma.potential.deleteMany();
 
     return NextResponse.json(
       {
