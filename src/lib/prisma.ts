2f5df9a4820225efa1ccfa48a20387dfc4d35e7d
HenriqueFLTorres <ricalisboa@gmail.com>
2024-02-28 15:50:00 -0300

    feat - update prisma client with best practices

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,15 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-//
-// Learn more:
-// https://pris.ly/d/help/next-js-best-practices
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export default prisma;
 
-export default prisma
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
