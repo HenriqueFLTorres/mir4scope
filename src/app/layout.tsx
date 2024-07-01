d6637e16e0257886f06cdf7eec53575d94874105
Guilherme Menezes <guilhermedeoliveira.menezes@gmail.com>
2024-06-30 21:42:28 -0300

    feat - navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,53 @@
-import "@/app/globals.css"
-import type { Metadata } from "next"
-import { Rubik } from "next/font/google"
-import { Providers } from "@/components/providers"
-import { cn } from "@/lib/cn"
+import "@/app/globals.css";
+import { Providers } from "@/components/providers";
+import { Button } from "@/components/ui/elements/button";
+import { cn } from "@/lib/cn";
+import { Eye, Gamepad2, PieChart, ScrollText } from "lucide-react";
+import type { Metadata } from "next";
+import { Rubik } from "next/font/google";
 
-const rubik = Rubik({ subsets: ["latin"] })
+const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mir4 Scope",
   description: "Analysis and assistance tool for finding and buying mir4 NFTs",
-}
+};
 
 export default function RootLayout({
   children,
   modal,
 }: Readonly<{
-  children: React.ReactNode
-  modal: React.ReactNode
+  children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body
         className={cn(
           rubik.className,
-          "min-h-screen bg-gradient-to-br from-[#44356A] to-[#272039]"
+          "flex flex-col gap-4 min-h-screen bg-gradient-to-br from-[#44356A] to-[#272039]",
         )}
       >
+        <header className="flex flex-row items-center justify-center pb-3 py-2 w-full bg-black/20 text-white">
+          <nav className="flex relative max-w-screen-2xl w-full items-center justify-center">
+            <Button className="absolute left-0">Sign in</Button>
+            <div className="flex gap-3">
+              <Button>
+                <Eye /> Character Watcher
+              </Button>
+              <Button>
+                <PieChart /> Analytics
+              </Button>
+              <Button>
+                <Gamepad2 /> In-Game NFT's
+              </Button>
+              <Button>
+                <ScrollText /> NFT Logs
+              </Button>
+            </div>
+          </nav>
+        </header>
+
         <Providers>
           {modal}
           {children}
@@ -33,5 +55,5 @@ export default function RootLayout({
         </Providers>
       </body>
     </html>
-  )
+  );
 }
